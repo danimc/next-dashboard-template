f149b0409f3ce2856bd1455229d990361ac52e4e
Erick Mora <m.erickpeligrogmail.com>
2025-04-10 14:45:09 -0600

    add prop

--- src/components/shared/LinksProfile.tsx
+++ src/components/shared/LinksProfile.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link'
 
-const LinksProfile = ({text}) => {
+const LinksProfile = ({text, url}) => {
     return (
-
-        <Link href="#" className="block hover:bg-gray-100 px-4 py-2 text-gray-700 text-sm">
+        <Link href={url} className="block hover:bg-gray-100 px-4 py-2 text-gray-700 text-sm">
                 {text}
         </Link>
-
     )
 }
 
