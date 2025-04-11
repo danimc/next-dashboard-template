6ab00fd0cf12b23942c0e021e421166a5614ff0b
Erick Mora <m.erickpeligrogmail.com>
2025-04-11 14:36:20 -0600

    templates profile & settings

---
+++ src/app/(dashboard)/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+    return (
+      <section className="justify-items-center  ">
+        <h1>Estás en tu perfil</h1>
+      </section>
+    );
+  }
---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+    return (
+        <section className="justify-items-center  ">
+          <h1>Estás en los ajustes</h1>
+        </section>
+      );
+}
